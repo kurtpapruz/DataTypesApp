3a9dec26f758f51d8c2b5edaa714893fb84f7c6b
papruz.289857 <papruz.289857@LAB-B-PC12>
2024-02-16 09:32:20 +0800

    Add project files.

---
+++ DataTypesApp/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace DataTypesApp
+{
+    class DataTypesProgram
+    {
+        static void Main(string[] args)
+        {
+        Console.Write("Enter the pieces of apple: ");
+            int pieces;
+            pieces = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter total price of " + pieces + " apples");
+            double price;
+            price = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("The total price of " + pieces + " is: " + price);
+            Console.WriteLine("The value of original price is " + price);
+            Console.WriteLine("The value of converted price is " + (int)price);
+
+        }
+    }
+}
